a618680b30db13f8761278a801a2c213b47cf5a3
FlorianBoldt <FlorianBoldt@0-208-11>
2021-09-28 15:25:31 +0200

    Dienstag Vertex

--- Javawillnurspielen/src/TestVertex.java
+++ Javawillnurspielen/src/TestVertex.java
@@ -4,13 +4,20 @@ public class TestVertex {
 	public static void main(String[] args) {
 		Vertex v1 = new Vertex(17,4);
 		Vertex v2 = new Vertex(42,0);
-		Vertex v3 = new Vertex(0,0);
+		Vertex v3 = v1.skalarMult(16);
+		Vertex maus = new Vertex(15,22);
+		Vertex katze = new Vertex(2,2);
+		Vertex hund = katze.add(maus);
+		
 		
  System.out.println("v1: "+v1);
  System.out.println("v2: "+v2);
  System.out.println("v3: "+v3);
- 
-
+ System.out.println(v1.length());
+ System.out.println(katze);
+ katze.addMod(maus);
+ System.out.println(katze);
+ System.out.println(hund);
 	}
 
 }
