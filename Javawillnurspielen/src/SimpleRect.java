64936de135909560c646ac993972f706b32b906c
FlorianBoldt <FlorianBoldt@0-208-11>
2021-10-12 15:30:17 +0200

    SimpleClasses

--- Javawillnurspielen/src/SimpleRect.java
+++ Javawillnurspielen/src/SimpleRect.java
@@ -1,4 +1,21 @@
 
-public class SimpleRect {
+public class SimpleRect extends GeometricObject{
+
+	public SimpleRect(double w, double h, Vertex pos) {
+		super(w,h,pos);
+	}
+	public SimpleRect(double w, double h, double x, double y) {
+		super(w,h, new Vertex(x,y));
+	}
+	@Override public double area() {
+		return width*height;
+	}
+	public @Override String toString() {
+		return "SimpleRect("+super.toString()+")";
+	}
+	public @Override boolean equals(Object that) {
+		return (that instanceof SimpleRect) && super.equals(that);
+	}
+	
 
 }
--- Javawillnurspielen/src/SimpleTriangle.java
+++ Javawillnurspielen/src/SimpleTriangle.java
@@ -1,4 +1,21 @@
 
-public class SimpleTriangle {
+public class SimpleTriangle extends GeometricObject{
+
+	public SimpleTriangle (double w, double h, Vertex pos) {
+		super(w,h,pos);
+	}
+	public SimpleTriangle (double w, double h, double x, double y) {
+		super(w,h, new Vertex(x,y));
+	}
+	@Override public double area() {
+		return ((width*width)/4)*Math.sqrt(3);
+	}
+	public @Override String toString() {
+		return "SimpleTriangle ("+super.toString()+")";
+	}
+	public @Override boolean equals(Object that) {
+		return (that instanceof SimpleTriangle ) && super.equals(that);
+	}
+	
 
 }
